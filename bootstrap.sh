3fbd8c8c390253a0659916f1e05616099b8c2d86
dyindude <dyindude@github.dyindude.net>
2017-10-28 16:57:32 -0500

    testing a preseed file

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 apt-get update
-apt-get install software-properties-common
-apt-add-repository ppa:ansible/ansible
+apt-get install software-properties-common -y
+apt-add-repository ppa:ansible/ansible -y
 apt-get update
-apt-get install ansible
+apt-get install ansible -y
 ansible-playbook -i inventory provision.yml
