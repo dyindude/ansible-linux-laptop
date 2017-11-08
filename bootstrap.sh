38256d72a6233d6ae2c441f074291a0cbe9d8117
dyindude <dyindude@github.dyindude.net>
2017-11-08 17:43:35 -0600

    added some more vital packages to vars/main

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,4 +4,4 @@ apt-get install software-properties-common -y
 apt-add-repository ppa:ansible/ansible -y
 apt-get update
 apt-get install ansible -y
-ansible-playbook -i inventory provision.yml --extra-vars "ran_from_vagrant=true"
+ansible-playbook -i inventory provision.yml
